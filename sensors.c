e6e80dfe4a980432952ec58619bfb81bdd7519e6
Mateo <numero2342@gmail.com>
2024-07-16 14:24:32 +0200

    fonction sensors

---
+++ sensors.c
@@ -0,0 +1,14 @@
+int i;
+int val;
+int redpin=0;
+void setup()
+{
+  pinMode(redpin,OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  i=analogRead(redpin);
+  val=(6762/(i-9))-4;
+  Serial.println(val);
+}
