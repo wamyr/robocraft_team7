b505aa14e41c215e4d5d534700acfa4f85411ddb
snowlem22 <148462878+snowlem22@users.noreply.github.com>
2024-07-16 15:32:41 +0200

    Update sensors.c

--- sensors.c
+++ sensors.c
@@ -1,88 +1,15 @@
-https://www.makerguides.com/sharp-gp2y0a21yk0f-ir-distance-sensor-arduino-tutorial/
-
-int i;
-int val;
-int redpin=0;
-void setup()
-{
-  pinMode(redpin,OUTPUT);
-  Serial.begin(9600);
-}
-void loop()
-{
-  i=analogRead(redpin);
-  val=(6762/(i-9))-4;
-  Serial.println(val);
-}
-
-// test annexe 
-
-/* SHARP GP2Y0A21YK0F IR distance sensor with 
-   Arduino and SharpIR library example code. 
-   More info: https://www.makerguides.com */
-
-// Include the library:
 #include "SharpIR.h"
 
-// Define model and input pin:
-#define IRPin A0
+// Définir les modèles et les broches d'entrée :
+#define IRPinFront A2
+#define IRPinRight A3
+#define IRPinLeft A4
 #define model 1080
 
-// Create variable to store the distance:
-int distance_cm;
-
-/* Model :
-   GP2Y0A02YK0F --> 20150
-   GP2Y0A21YK0F --> 1080
-   GP2Y0A710K0F --> 100500
-   GP2YA41SK0F  --> 430
-*/
-
-// Create a new instance of the SharpIR class:
-SharpIR mySensor = SharpIR(IRPin, model);
-
-void setup() {
-  // Begin serial communication at a baudrate of 9600:
-  Serial.begin(9600);
-}
-
-void loop() {
-  // Number of readings to take for averaging
-  const int numReadings = 10;
-  int totalDistance = 0;
-
-  for (int i = 0; i < numReadings; i++) {
-    int reading = mySensor.distance();
-    if (reading > 0) { // Check if the reading is valid
-      totalDistance += reading;
-    } else {
-      Serial.println("Invalid reading");
-    }
-    delay(50); // Short delay between readings
-  }
-
-  // Calculate the average distance
-  distance_cm = totalDistance / numReadings;
-
-  // Print the measured distance to the serial monitor:
-  Serial.print("Mean distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
-
-  delay(1000); // Delay before the next measurement
-}
-
-//fin
-//nouveau 
-
-#include "SharpIR.h"
-
-// Définir le modèle et la broche d'entrée :
-#define IRPin A4
-#define model 1080
-
-// Créer une variable pour stocker la distance :
-int distance_cm;
+// Créer des variables pour stocker les distances en millimètres :
+int distance_mmFront;
+int distance_mmRight;
+int distance_mmLeft;
 
 /* Modèles pris en charge :
    GP2Y0A02YK0F --> 20150
@@ -91,8 +18,10 @@ int distance_cm;
    GP2YA41SK0F  --> 430
 */
 
-// Créer une nouvelle instance de la classe SharpIR :
-SharpIR mySensor = SharpIR(IRPin, model);
+// Créer des nouvelles instances de la classe SharpIR pour chaque capteur :
+SharpIR sensorFront = SharpIR(IRPinFront, model);
+SharpIR sensorRight = SharpIR(IRPinRight, model);
+SharpIR sensorLeft = SharpIR(IRPinLeft, model);
 
 void setup() {
   // Initialiser la communication série à 9600 baud :
@@ -100,13 +29,28 @@ void setup() {
 }
 
 void loop() {
-  // Obtenir une mesure de distance et la stocker dans distance_cm :
-  distance_cm = mySensor.distance();
+  // Obtenir une mesure de distance en centimètres pour chaque capteur :
+  int distance_cmFront = sensorFront.distance();
+  int distance_cmRight = sensorRight.distance();
+  int distance_cmLeft = sensorLeft.distance();
 
-  // Afficher la distance mesurée sur le moniteur série :
-  Serial.print("Distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
+  // Convertir les distances en millimètres :
+  distance_mmFront = distance_cmFront * 10;
+  distance_mmRight = distance_cmRight * 10;
+  distance_mmLeft = distance_cmLeft * 10;
+
+  // Afficher les distances mesurées sur le moniteur série :
+  Serial.print("Distance devant : ");
+  Serial.print(distance_mmFront);
+  Serial.println(" mm");
+
+  Serial.print("Distance à droite : ");
+  Serial.print(distance_mmRight);
+  Serial.println(" mm");
+
+  Serial.print("Distance à gauche : ");
+  Serial.print(distance_mmLeft);
+  Serial.println(" mm");
 
   // Délai avant la prochaine mesure :
   delay(100);
