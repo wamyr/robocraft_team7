8d5c2942a69b5194f229d633df2e4f6466f7f634
Carpe-bleue <88663397+Carpe-bleue@users.noreply.github.com>
2024-07-16 16:16:37 +0200

    Update motors.c

--- motors.c
+++ motors.c
@@ -1,1 +1,18 @@
+#define dir_motor_1 5
+#define dir_motor_2 6
+#define PWM_pin_1 4
+#define PWM_pin_2 9
+
+void setup(){}
+void loop() {
+  // Accelerate forward
+  digitalWrite(dir_motor_1, LOW);
+  digitalWrite(dir_motor_2, LOW);
+  int speed = 250;
+  analogWrite(PWM_pin_1, speed);
+  analogWrite(PWM_pin_2, speed);
+  delay(50);
+  
+  delay(2000);  // Wait for 2 seconds before repeating
+}
 
