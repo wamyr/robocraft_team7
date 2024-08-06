f571416676e3837410fc4bed2af6c44cf5a958ae
Carpe-bleue <88663397+Carpe-bleue@users.noreply.github.com>
2024-08-06 12:04:31 +0200

    Update navigation_file.py

--- navigation_file.py
+++ navigation_file.py
@@ -1,39 +1,43 @@
 #!/usr/bin/env python3
+
+# chmod +x
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
 class ReactiveNavigation():
-	def __Init__(self):
-		self.cmd_vel = Twist()
-		self.laser_msg = LaserScan()
-		self.robot_stopped = False
-		self.obstacle_distance = 100
-		self.rospy_sub_laser = rospy.Subscriber("base_scan",LaserScan,self.laser_cb,queue_size = 1)
-		self.pub_CMD = rospy.Publisher("cmd_vel",Twist,queue_size = 1)
-		self.rate = rospyRate(5)
-		
-	def laser_cb(self, callback):
-		self.laser_msg = callback
-	
-	def calculate_command(self):
-		if type(self.laser_msg.ranges)==tuple:
-			self.obstacle_distance = min(self.laser_msg.ranges)
-			
-			if self.obstacle_distance > 1.0:
-				self.cmd_vel.linear.x = 1.0
-				self.cmd_vel.angular.z = 0.0
-			else:
-				self.cmd_vel.linear.x = 0.0
-				self.cmd_vel.angular.z = 1.0
-	def run(self):
-		
-		while not rospy.is_shutdown :
-			self.calculate_command()
-			self.pub_CMD.publish(self.cmd_vel)
-			self.rate.sleep()
-		
-if __name__ == "__main__":
-	rospy.init_node("reactive_controller.py")
-	controller = ReactiveNavigation()
-	controller.run()
+    def __init__(self):
+        self.cmd_vel = Twist()
+        self.laser_msg = LaserScan()
+        self.robot_stopped = False
+        self.obstacle_distance = 100
+        self.rospy_sub_lazer = rospy.Subscriber("base_scan", LaserScan, self.laser_bc, queue_size=1)
+        self.pub_CMD = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.rate = rospy.Rate(5)
+
+    def laser_bc(self, callback):
+        self.laser_msg = callback
+    
+    def calculate_command(self):
+        if type(self.laser_msg.ranges) == tuple:
+            self.obstacle_distance = min(self.laser_msg.ranges)
+
+            if self.obstacle_distance > 1.0:
+                self.cmd_vel.linear.x = 1.0
+                self.cmd_vel.angular.z = 0.0
+            else:
+                self.cmd_vel.linear.x = 0.0
+                self.cmd_vel.angular.z = 1.0
+
+            self.obstacle_distance = 100
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.calculate_command()
+            self.pub_CMD.publish(self.cmd_vel)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('reactive_navigation_py')
+    controller = ReactiveNavigation()
+    controller.run()
