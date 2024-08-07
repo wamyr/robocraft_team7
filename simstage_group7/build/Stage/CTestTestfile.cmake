714138425f114b09f2adc45eb70feb9d54c5854a
snowlem22 <your_email@example.com>
2024-08-07 15:25:52 +0100

    sim pack

---
+++ simstage_group7/build/Stage/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/grimont/Documents/ROS1/simstage_group7/src/Stage
+# Build directory: /home/grimont/Documents/ROS1/simstage_group7/build/Stage
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ simstage_group7/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PWD='/home/grimont/Documents/ROS1/simstage_group7/build'
